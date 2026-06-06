a5c7c9009ded9fbbafce3499ec027768a8033e39
Dno-0749 <hainc0749@ut.edu.vn>
2026-06-06 09:53:07 +0700

    Add files via upload

---
+++ Code/src/server/Server.java
@@ -0,0 +1,36 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static final int PORT = 1234;
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+System.out.println("=================================");
+            System.out.println(" SERVER IS RUNNING...");
+            System.out.println(" Port: " + PORT);
+            System.out.println("=================================");
+            while (true) {
+
+                Socket socket = serverSocket.accept();
+
+                String clientIP =
+                        socket.getInetAddress().getHostAddress();
+
+                System.out.println(
+                        "New client connected: " + clientIP);
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Server error!");
+            e.printStackTrace();
+        }
+    }
+}
